119f9583a563b3ea30e47054b543146a3be2a0f7
Konrad Amtenbrink <konrad02@gmx.de>
2022-11-10 15:04:20 +0100

    feat: add directory traversal with keyword match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use home::home_dir;
+use std::env;
+use std::io;
+use std::path::{Path, PathBuf};
+use walkdir::{DirEntry, WalkDir};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    path: std::path::PathBuf,
+}
+
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| entry.depth() == 0 || !s.starts_with("."))
+        .unwrap_or(false)
+}
+
+fn handle_dir(dir: &DirEntry, search: &String) {
+    if dir
+        .path()
+        .to_str()
+        .unwrap()
+        .contains(format!("/{}/", search).as_str())
+    {
+        println!("{}", dir.path().display())
+    }
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let path = &args.path.into_os_string().into_string().unwrap();
+
+    let root = home_dir().unwrap();
+    WalkDir::new(root)
+        .into_iter()
+        .filter_entry(|e| is_not_hidden(e))
+        .filter_map(|v| v.ok())
+        .for_each(|x| handle_dir(&x, path));
+    Ok(())
 }
